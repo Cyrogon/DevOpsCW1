cb54a63873bc56561f3ecda520c5d60bae8cf41c
Cyrogon <45185673+Cyrogon@users.noreply.github.com>
2024-11-06 21:18:35 +0000

    Changed hexadeimal to String Builder
    Updated file structure and changed Hexadecimal to be a string builder

--- converters/Dec2Hex.java
+++ converters/Dec2Hex.java
@@ -2,20 +2,21 @@ package converters;
 
 class Dec2Hex
 {
-public static int Arg1;
+public static int arg1;
 
     public static String convDec(String[] args) {
-        Arg1 = Integer.parseInt(args[0]);
+        arg1 = Integer.parseInt(args[0]);
         char[] ch ={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
+        int rem;
+        int num;
+        num = arg1;
+        StringBuilder hexadecimal= new StringBuilder();
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
         {
             rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
+            hexadecimal.insert(0, ch[rem]);
             num= num/16;
         }
 
