d39999a7d28ffadf23bf7639a1e62cec6e8c213a
Cyrogon <45185673+Cyrogon@users.noreply.github.com>
2024-11-06 21:24:32 +0000

    Removed Needless arg1 var

--- converters/Dec2Hex.java
+++ converters/Dec2Hex.java
@@ -2,14 +2,11 @@ package converters;
 
 class Dec2Hex
 {
-public static int arg1;
 
     public static String convDec(String[] args) {
-        arg1 = Integer.parseInt(args[0]);
+        int num = Integer.parseInt(args[0]);
         char[] ch ={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem;
-        int num;
-        num = arg1;
         StringBuilder hexadecimal= new StringBuilder();
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
